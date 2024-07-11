aef242846541efbf20e2c783c5dd477362902a23
HariR11 <9677106274h@gmail.com>
2024-07-11 23:12:12 +0530

    ex1

---
+++ hackerrank/ex1.cpp
@@ -0,0 +1,10 @@
+// Say "Hello, World!" With C++
+//https://www.hackerrank.com/challenges/cpp-hello-world?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<< "Hello, World!";
+    return 0;
+}
