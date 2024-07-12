6984399ed8b55c536c6e229ffef317a651cf7fff
HariR11 <9677106274h@gmail.com>
2024-07-12 20:37:42 +0530

    basicdatatypes

---
+++ learning/Datatypes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    long y; 
+    float r;
+    long long z;
+    string str;
+    getline (cin,str);
+    cin >> x>>y>>r>>z;
+    cout<<x<<"\n"<<y<<"\n"<<r<< "\n"<<z<<"\n"<<str;
+    return 0;
+
+
+}
